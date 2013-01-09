3935269791b2ceb966a7714ce1c452bfcd238839
Adrian Engel <adihimself@gmail.com>
2013-01-09 00:44:39 -0800

    Create jquery.slwdwn.js

---
+++ jquery.slwdwn.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+  jQuery('.image a').fancybox();
+	
+	var slwdwnSpeed = 1000; // 2000 = 2 seconds; 1000 = 1 seconds
+	var slwdwnElement = 'a.slwdwn';
+
+	jQuery(slwdwnElement).each(function(e){
+		jQuery(this).click(function(e){
+			var getID = $(this).attr('href');
+			var offset = $(getID).offset();
+			jQuery('html,body').animate({
+				scrollTop: offset.top	
+			},slwdwnSpeed);
+			return false;
+		});
+	});
+});
